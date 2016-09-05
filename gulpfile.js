4431ee6b3d43cbaa10707d54a7fb477daa2f0494
Eric Caleb <calebme@mail.com>
2016-09-05 11:24:03 +0100

    Added html js and css gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = requie('gulp'),
+    browserify = require('gulp-browserify'),
+    webserver  = require('gulp-webserver');
+
+var src =  './process',
+    app =  './app';
+
+gulp.tast('js' , function(){
+    return gulp.src(src + '/js/app.js')
+    .pipe(browserify({
+        transform: 'reactify',
+        debug: true
+    }))
+    .on('error' ,function(err){
+        console.error('Error!' ,err.message)
+
+    })
+    .pipe(gulp.dest(app + '/js'));
+});
+
+
+gulp.task('html' ,function(){
+    gulp.src(app + "/**/*.html");
+})
+
+gulp.task('css' , function(){
+    gulp.src(app + '/css/*.css');s
+})
+
