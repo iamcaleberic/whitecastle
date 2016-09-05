26c138111e6f574af5cfcd88a7164e0ac4974d6c
Eric Caleb <calebme@mail.com>
2016-09-05 12:43:29 +0100

    Modified app.js and removed old function data

--- process/js/app.js
+++ process/js/app.js
@@ -4,28 +4,14 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
     getInitialState: function(){
         return {
-            title: ' Famous Cities' ,
-            show: true
+        
         }
     },
     render: function(){
-        var showTitle;
-        if(this.state.show){
-            showTitle = "Less"
-        }
-
-        var displayList ={
-            display: this.state.show ? 'block':none,
-            color: 'red'
-        }
+        
         return (
             <div className="World">
-            <h1 > { showTitle }
-                {this.state.title}  </h1>
             
-                <ul style= {displayList}>
-                    <li>London (BST)</li>
-                </ul>
             </div>
             )
     }
