e029e3926e02716732bfd2329a3bb96e21f11397
Eric Caleb <calebme@mail.com>
2016-09-05 12:23:55 +0100

    Added state function to main component

--- process/js/app.js
+++ process/js/app.js
@@ -2,10 +2,15 @@ var React =  require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
+    getInitialState: function(){
+        return {
+            title: 'Famous Cities' 
+        }
+    },
     render: function(){
         return (
             <div className="World">
-            <h1 > World Cities </h1>
+            <h1 > { this.state.title } </h1>
             
                 <ul>
                     <li>London (BST)</li>
