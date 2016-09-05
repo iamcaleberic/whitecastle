d9a26acb885c7893de1234fe6be6595199eb8a3f
Eric Caleb <calebme@mail.com>
2016-09-05 12:32:28 +0100

    Adding ternary expressions for more complex expressions to the DOM

--- process/js/app.js
+++ process/js/app.js
@@ -4,13 +4,19 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
     getInitialState: function(){
         return {
-            title: 'Famous Cities' 
+            title: ' Famous Cities' ,
+            show: true
         }
     },
     render: function(){
+        var showTitle;
+        if(this.state.show){
+            showTitle = "Less"
+        }
         return (
             <div className="World">
-            <h1 > { this.state.title } </h1>
+            <h1 > { showTitle }
+                {this.state.title}  </h1>
             
                 <ul>
                     <li>London (BST)</li>
