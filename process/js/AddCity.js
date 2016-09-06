b59f48ccc630595bb442a46c447a2d5d0e08c728
Eric Caleb <calebme@mail.com>
2016-09-06 10:54:47 +0100

    Added addcity form to AddCity subcomponent

--- process/js/AddCity.js
+++ process/js/AddCity.js
@@ -2,6 +2,29 @@ var React =  require('react');
 
 var AddCity =  React.createClass({
     render: function(){
-        return ()
+        return (
+            <div className="ui basic segemnt"> 
+                <div className="ui container">
+                    <form class="ui form">
+                        <div className="field">
+                            <label>City</label>
+                            <input name="city" placeholder="City" type="text"/>
+                        </div>
+                        <div className="field">
+                            <label>Description</label>
+                            <input name="Description" placeholder="Description" type="text"/>
+                        </div>
+                        <div className="field">
+                            <label>Country</label>
+                            <input name="Country" placeholder="Country" type="text"/>
+                        </div>
+                    
+                        <button className="ui button" type="submit">Submit</button>
+                    </form>
+                </div>
+              </div>
+        )
     }
-})
+})//add city
+
+module.exports = AddCity
